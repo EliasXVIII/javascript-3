7e1aba220056ccda2faebfc3117ed7dc3d4fea56
johanrodriguez24 <srodriguez5860@gmail.com>
2024-02-01 13:34:44 +0100

    script slider_sebastian

--- sebas.js
+++ sebas.js
@@ -1,1 +1,36 @@
-//prueba de escritura
+// ----------------EXPLICACIÓN SIMPLE EN CONSOLA------------------------
+
+
+//Definiendo el intervalo de tiempo
+
+let segundos = 10;
+
+//Definiendo el tipo de ejercicio
+
+let aplauso = function(){
+    segundos --
+    console.log(`Doy un aplauso`)
+    console.log (segundos)
+
+//clearInterval(expresiones)
+
+if (segundos ===0)
+{clearInterval(expresiones)}
+}
+
+//Defino la acción 
+
+let expresiones = setInterval(aplauso, 1000);
+
+//Establecer límites en las expresiones
+
+
+
+
+//------------------Slider--------------------
+
+
+//const slider
+
+
+
